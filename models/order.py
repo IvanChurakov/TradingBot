f943f12294a4f8c3f4a9f65769ecfdd24b5744c1
Ivan Churakov <ichurakov@foundationmedicine.com>
2025-04-13 21:36:48 +0300

    wip

--- models/order.py
+++ models/order.py
@@ -1,20 +1,19 @@
-from decimal import Decimal
-from typing import Union, Optional
+from typing import Optional
 
 
 class Order:
     def __init__(
             self,
             order_link_id: str,
-            amount: Union[float, Decimal],
-            price: Union[float, Decimal],
+            amount: float,
+            price: float,
             allow_to_sell: bool = False,
             action: str = "",
             buy_order_link_id: Optional[str] = None
     ):
         self.order_link_id = order_link_id
-        self.amount = Decimal(str(amount)) if isinstance(amount, float) else amount
-        self.price = Decimal(str(price)) if isinstance(price, float) else price
+        self.amount = amount
+        self.price = price
         self.allow_to_sell = allow_to_sell
         self.action = action
         self.buy_order_link_id = buy_order_link_id
@@ -31,7 +30,7 @@ class Order:
     def from_dict(cls, data: dict):
         return cls(
             order_link_id=data["orderLinkId"],
-            amount=float(data["amount"]) if isinstance(data["amount"], Decimal) else data["amount"],
-            price=float(data["price"]) if isinstance(data["price"], Decimal) else data["price"],
+            amount=float(data["amount"]),
+            price=float(data["price"]),
             allow_to_sell=data.get("allowToSell", False)
         )
